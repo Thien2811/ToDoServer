9df1f002ed3bc4329e1e675ce9642652f7228fb9
thien2811 <t.nguyen@henova.de>
2024-02-05 12:18:39 +0100

    Stand 05.02 alles überarbeitet

--- myapp.ts
+++ myapp.ts
@@ -2,8 +2,25 @@ import express from "express";
 import bodyParser from "body-parser";
 import mysql from "mysql";
 import cors from "cors";
+import session from "express-session";
+import redis from "redis";
+import RedisStore from "connect-redis";
+import cookieParser from "cookie-parser";
 
 require("dotenv").config();
+
+declare module "express-session" {
+  interface Session {
+    user: User;
+  }
+}
+
+type User = {
+  id: number;
+  name: string;
+  passHash: string;
+  loggedIn: boolean;
+};
 
 type Task = {
   id: number;
@@ -17,7 +34,34 @@ type Task = {
 
 const PORT = 5000;
 
+const redisClient = await redis
+  .createClient({
+    socket: {
+      host: "localhost",
+      port: 6379,
+    },
+  })
+  .on("error", (err) =>
+    console.warn("Fehler bei Erstellung von redisClient", err)
+  )
+  .on("connect", (err) => {
+    console.info(`Mit redis verbunden`);
+  })
+  .connect();
+
 const app = express();
+
+app.use(
+  session({
+    store: new RedisStore({ client: redisClient }),
+    secret: "123123123123123123123",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true,
+    },
+  })
+);
 
 const connection = mysql.createConnection({
   host: "localhost",
@@ -38,14 +82,31 @@ app.use(cors());
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 
+app.use(express.static("./spa"));
+
+// app.use((req, res, next) => {
+//   if (!req.session.user?.loggedIn) {
+//     //nicht eingeloggt
+//     if (req.route === "/login") {
+//       //route ist ein login
+//     } else {
+//       res.status(401).end();
+//       //request ist kein login
+//     }
+//   } else {
+//     next();
+//     //ist eingeloggt
+//   }
+// });
+
 app.get("/", (req, res) => {
   res.send("Thiens ToDo!");
 });
 
 app.post("/addlist", (req, res) => {
-  const data = req.body.name;
+  const data = req.body.list[0];
   connection.query(
-    `INSERT INTO lists (listname) VALUES ('${data}')`,
+    `INSERT INTO lists (listname, uuid) VALUES ('${data.listname}','${data.uuid}')`,
     (error, results) => {
       if (error) throw error;
       res.status(200).json(results).end();
@@ -54,7 +115,7 @@ app.post("/addlist", (req, res) => {
 });
 
 app.get("/getlistnames", (req, res) => {
-  connection.query(`SELECT listname FROM lists`, (error, results) => {
+  connection.query(`SELECT listname,uuid FROM lists`, (error, results) => {
     if (error) throw error;
     res.status(200).json(results);
   });
@@ -71,6 +132,17 @@ app.post("/gettasks", (req, res) => {
   );
 });
 
+// app.get("/login", (req, res) => {
+//   const dbUser = db.getuser;
+//   if (verified) {
+//     req.session.user = {
+//       id: dbUser.id,
+//       name: dbUser.name,
+//       pass_hash: dbUser.pass_hash,
+//     };
+//   }
+// });
+
 app.delete("/task/:id", (req, res) => {
   const id = req.params.id;
   connection.query(`DELETE FROM tasks WHERE id=${id}`);
@@ -79,9 +151,15 @@ app.delete("/task/:id", (req, res) => {
 
 app.delete("/deletelist/:deletedlistname", (req, res) => {
   const data = req.params.deletedlistname;
-  console.log(data);
   connection.query(
     `DELETE FROM lists WHERE listname='${data}'`,
+    (error, results) => {
+      if (error) throw error;
+      res.status(200).json(results).end();
+    }
+  );
+  connection.query(
+    `DELETE FROM tasks WHERE listname='${data}'`,
     (error, results) => {
       if (error) throw error;
       res.status(200).json(results).end();
@@ -97,7 +175,7 @@ app.post("/addtask", (req, res) => {
     : "NULL";
 
   connection.query(
-    `INSERT INTO tasks (listname, taskname, description, user, datum, priority) VALUES ('${data.listname}','${data.taskname}','${data.description}','${data.user}',${datum},'${data.priority}')`,
+    `INSERT INTO tasks (listname, taskname, description, user, datum, priority, uuid) VALUES ('${data.listname}','${data.taskname}','${data.description}','${data.user}',${datum},'${data.priority}','${data.uuid}')`,
     (error, results) => {
       if (error) throw error;
       res.status(200).json(results).end();
@@ -111,11 +189,25 @@ app.post("/save", (req, res) => {
     ? `"${task.date.split(".").reverse().join("-")}"`
     : "NULL";
   connection.query(
-    `UPDATE tasks SET taskname='${task.taskname}',description='${task.description}',user='${task.user}',datum=${datum},priority='${task.priority}' WHERE id=${task.id}`,
+    `UPDATE tasks SET taskname='${task.taskname}',description='${task.description}',user='${task.user}',datum=${datum},priority='${task.priority}',uuid='${task.uuid}' WHERE id=${task.id}`,
     (error, results) => {
       if (error) throw error;
       res.status(200).json(results).end();
     }
+  );
+});
+
+app.post("/savelistname", (req, res) => {
+  const list = req.body;
+  connection.query(
+    `UPDATE lists SET listname='${list.listname}' WHERE uuid='${list.id}'`,
+    (error, results) => {
+      if (error) throw error;
+      res.status(200).json(results).end();
+    }
+  );
+  connection.query(
+    `UPDATE tasks SET listname='${list.listname}' WHERE uuid='${list.id}'`
   );
 });
 
---
+++ spa/assets/start.536a12c3.js
@@ -0,0 +1,1 @@
+import{a}from"./axios.6b484fa5.js";import{b as t}from"./index.49a26021.js";var r=t(async()=>{a.defaults.baseURL="http://localhost:5000"});export{r as default};
